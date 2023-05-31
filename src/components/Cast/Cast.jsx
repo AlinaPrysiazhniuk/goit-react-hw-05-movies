3f6dcc114fcb68a2426cfff5d24a99f45344fef1
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-31 15:38:56 +0100

    cast styles

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,7 @@
 import { searchMovieCredits } from 'components/SearchMovies';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import css from './Cast.module.css';
 
 const Cast = () => {
   const { movieId } = useParams('movieId');
@@ -11,16 +12,16 @@ const Cast = () => {
   }, [movieId]);
 
   return (
-    <div>
+    <div className={css.cast_container}>
       {credits.length > 0 && (
-        <ul>
+        <ul className={css.cast_list}>
           {credits.map(({ id, name, character, photo }) => {
             return (
-              <li key={id}>
-                <img src={photo} alt={name} />
-                <div>
-                  <p>{name}</p>
-                  <p>Character: {character}</p>
+              <li key={id} className={css.cast_item}>
+                <img src={photo} alt={name} className={css.cast_img} />
+                <div className={css.cast_description}>
+                  <p className={css.cast_name}>{name}</p>
+                  <p className={css.cast_character}>Character: {character}</p>
                 </div>
               </li>
             );
