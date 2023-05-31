93c099523259b05f3af0b274c8386dba0d633e9c
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-31 16:09:35 +0100

    layout styles

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,17 +2,20 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { NavLink } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
+import css from './layout.module.css';
 
 const Layout = () => {
   return (
     <>
-      <div>
-        <NavLink to="/">Home</NavLink>
+      <div className={css.layout_nav}>
+        <NavLink to="/" className={css.layout_link}>
+          Home
+        </NavLink>
         <NavLink to="/movies">Movies</NavLink>
       </div>
 
-      <div>
-        <Suspense fallback={<Loader />}>
+      <div className={css.layout_container}>
+        <Suspense fallback={<Loader />} className>
           <Outlet />
         </Suspense>
       </div>
