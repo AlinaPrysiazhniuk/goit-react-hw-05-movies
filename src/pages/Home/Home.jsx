3b2c71ec1059470cdee8785dac9fd8c9be5ed3e6
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-31 15:52:57 +0100

    home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { searchTranding } from 'components/SearchMovies';
 import { Link } from 'react-router-dom';
+import css from './Home.module.css';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
@@ -11,19 +12,25 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <h1>Trandindg today</h1>
-      <ul>
-        {movies.map(({ id, title, poster }) => (
-          <li key={id}>
-            <Link to={`/movies/${id}`} state={{ from: location }}>
-              <img src={poster} alt={title} />
-              <h3>{title}</h3>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className={css.home_container}>
+      <h1 className={css.home_title}>Trandindg today</h1>
+      {movies.length > 0 && (
+        <ul className={css.movie_list}>
+          {movies.map(({ id, title, poster }) => (
+            <li key={id} className={css.movie_item}>
+              <Link
+                to={`/movies/${id}`}
+                state={{ from: location }}
+                className={css.movie_link}
+              >
+                <img src={poster} alt={title} className={css.movie_img} />
+                <h3 className={css.movie_title}>{title}</h3>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
